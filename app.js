7d55f22d4ff9e7f002401fa4e1fa131155e89624
MANZA <36519974+zaimanza@users.noreply.github.com>
2023-07-25 11:31:34 +0800

    deleting

--- app.js
+++ app.js
@@ -1,35 +1,12 @@
-const express = require("express")
-require("dotenv").config();
-var cors = require('cors')
-const app = express()
-const bodyParser = require("body-parser")
+const express = require("express");
+const app = express();
 
+const port = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({
-  extended: true,
-  limit: '2000mb',
-  parameterLimit: 10000000000
-}));
-app.use(bodyParser.json({ limit: '2000mb' }));
-app.use(cors())
-app.use(require("./middleware/token/decode.token"))
-
-// Routes which should handle requests
-app.use("/", require("./controllers"))
-
-app.use((req, res, next) => {
-  const error = new Error("Not found");
-  error.status = 404;
-  next(error);
-});
-
-app.use((error, req, res, next) => {
-  res.status(error.status || 500);
+app.get("/", (req, res) => {
   res.json({
-    error: {
-      message: error.message
-    }
+    hi: "hello",
   });
 });
 
-module.exports = app;
+app.listen(port);
--- controllers/auth/index.js
+++
@@ -1,6 +0,0 @@
-var router = require('express').Router()
-
-// split up route handling
-router.use('/', require('./login.controller'))
-
-module.exports = router
